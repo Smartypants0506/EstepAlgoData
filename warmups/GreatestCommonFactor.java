5f97eec4472f3e07c00afeae249b3c2ffd3749e2
Smartypants0506 <omkar.bantanur@gmail.com>
2024-10-11 12:30:59 -0400

    test

--- warmups/GreatestCommonFactor.java
+++ warmups/GreatestCommonFactor.java
@@ -1,1 +1,19 @@
+public class GreatestCommonFactor {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isCommonFactor(int f, int num1, int num2) {
+        return num1 % f == 0 && num2 % f == 0;
+    }
+
+    public static int gcf(int n, int m) {
+        int count = n;
+        while(!isCommonFactor(count, n, m)) {
+            count--;
+        }
+        return count;
+    }
+}
 
